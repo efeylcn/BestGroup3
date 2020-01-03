6b6c20ddd13d2a49f0a17a136fbb0a857abc1422
NARGIZA001 <nkeneshbaeva1@gmail.com>
2020-01-02 20:50:56 -0600

    Api package with classes added

--- src/test/java/Pages/HomePage.java
+++ src/test/java/Pages/HomePage.java
@@ -3,7 +3,7 @@ package Pages;
 public class HomePage {
 
     public static void main(String[] args) {
-        System.out.println("hello git, project should be fine by now");
+        System.out.println(" project should be fine by now");
     }
 
 }
